637c4a26d274d95e7fcfbcbfe8f621e41497f120
R3IS <101276269+xreiz0r@users.noreply.github.com>
2024-02-07 19:39:30 +0000

    Create exitr.sh

---
+++ exitr.sh
@@ -0,0 +1,14 @@
+#Linux Ver
+
+
+#! /bin/sh
+
+
+echo '' > ~/.local/share/recently-used.xbel
+echo '[+]File:recently-used'
+sleep .4
+
+echo '' > ~/.bash_history
+echo '[+]File:bash_history'
+sleep .4
+echo '\n[+]Clear'
